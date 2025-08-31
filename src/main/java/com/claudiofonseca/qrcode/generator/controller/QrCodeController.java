55a24a0c01917f188f9b4af54670c16eb0ce0b70
Cláudio Fonseca <139545495+CaluFonseca@users.noreply.github.com>
2025-08-31 01:55:31 +0100

    AWS configuration, dockerfile, .env and application.properties update

--- src/main/java/com/claudiofonseca/qrcode/generator/controller/QrCodeController.java
+++ src/main/java/com/claudiofonseca/qrcode/generator/controller/QrCodeController.java
@@ -2,6 +2,7 @@ package com.claudiofonseca.qrcode.generator.controller;
 
 import com.claudiofonseca.qrcode.generator.dto.QrCodeGenerateRequest;
 import com.claudiofonseca.qrcode.generator.dto.QrCodeGenerateResponse;
+import com.claudiofonseca.qrcode.generator.service.QrCodeGeneratorService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,8 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/qrcode")
 public class QrCodeController {
 
+    private final QrCodeGeneratorService qrCodeGeneratorService;
+
+    public QrCodeController(QrCodeGeneratorService qrCodeGeneratorService) {
+        this.qrCodeGeneratorService = qrCodeGeneratorService;
+    }
+
     @PostMapping
     public ResponseEntity<QrCodeGenerateResponse> generate(@RequestBody QrCodeGenerateRequest request) {
-        return null;
+        try {
+            QrCodeGenerateResponse response = this.qrCodeGeneratorService.generateAndUploadQrCode(request.text());
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
